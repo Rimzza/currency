98384d54ffee2ad5e53a04ca0fcab35d80110499
Nikita Egorov <mr.nik.egorov.98@mail.ru>
2023-12-06 16:53:26 +0300

    lil changes

--- src/main/java/by/rimza/bot/CurrencyBot.java
+++ src/main/java/by/rimza/bot/CurrencyBot.java
@@ -7,7 +7,6 @@ import by.rimza.service.CurrencyService;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
